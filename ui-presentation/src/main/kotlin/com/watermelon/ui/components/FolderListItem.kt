35599c7de85fd8de9ef6da5de3ac842b047092db
So Muzaffari <so.muzaff@gmail.com>
2026-06-09 06:01:53 +0330

    Add Color import to FolderListItem.kt

--- ui-presentation/src/main/kotlin/com/watermelon/ui/components/FolderListItem.kt
+++ ui-presentation/src/main/kotlin/com/watermelon/ui/components/FolderListItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import com.watermelon.ui.R
 import androidx.compose.runtime.Composable
